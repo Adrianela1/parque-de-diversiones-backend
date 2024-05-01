bbc923e77fdd34870e16011d7de365d4671dcbc7
Adrianela1 <nemesiososo@gmail.com>
2024-04-30 18:32:05 -0600

    initialize project

---
+++ src/test/java/com/msvc/proyectopark/ParqueDiversionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msvc.proyectopark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParqueDiversionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
