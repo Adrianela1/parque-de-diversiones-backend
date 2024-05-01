accb7b254355b4304b75aad8fb97653dd6f825c3
Adrianela1 <nemesiososo@gmail.com>
2024-04-30 18:52:50 -0600

    agregamos servicio compradores

---
+++ compradores-service/src/test/java/com/msvc/compradores/CompradoresServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msvc.compradores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompradoresServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
