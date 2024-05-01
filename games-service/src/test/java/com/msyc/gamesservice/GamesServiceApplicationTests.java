3ecaa051a55ed9c38bb472cf90a60d2db7ce266b
Abigail2507 <aby2507@gmail.com>
2024-05-01 17:40:59 -0600

    PDD-6 <agregamos servicio de juegos>

---
+++ games-service/src/test/java/com/msyc/gamesservice/GamesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msyc.gamesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
