9af4ffcfbebea68a4fcd4a9a7e9512bc819e161a
Adrianela1 <nemesiososo@gmail.com>
2024-05-02 19:17:26 -0600

    discovery server add

--- compradores-service/src/main/java/com/msvc/compradores/CompradoresServiceApplication.java
+++ compradores-service/src/main/java/com/msvc/compradores/CompradoresServiceApplication.java
@@ -2,8 +2,10 @@ package com.msvc.compradores;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CompradoresServiceApplication {
 
 	public static void main(String[] args) {
