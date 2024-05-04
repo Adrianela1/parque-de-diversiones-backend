e496f779abcf0bcf120b6929a8fc66eacd612fe0
Abigail2507 <aby2507@gmail.com>
2024-05-03 22:57:05 -0600

    PDD-6 <se agregaron entidades>

--- games-service/src/test/java/com/msyc/gamesservice/GamesModelServiceApplicationTests.java
+++ games-service/src/test/java/com/msyc/gamesservice/GamesModelServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GamesServiceApplicationTests {
+class GamesModelServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
