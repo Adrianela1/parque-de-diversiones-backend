6536111accca054859e3d9f5910b97e1894adec2
Dafneesanch23 <daaphsanchez@gmail.com>
2024-05-09 16:46:49 -0600

    Se añaden dependencias de Eureka Discovery Client a Tickets

--- tickets-service/src/main/java/com/msvc/tickets/TicketsServiceApplication.java
+++ tickets-service/src/main/java/com/msvc/tickets/TicketsServiceApplication.java
@@ -2,8 +2,10 @@ package com.msvc.tickets;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TicketsServiceApplication {
 
 	public static void main(String[] args) {
