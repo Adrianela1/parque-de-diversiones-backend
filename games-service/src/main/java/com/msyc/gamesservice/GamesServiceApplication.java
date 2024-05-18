dd8b4948b31f5f1f9d85832c7b573a3c2fca1f86
Abigail2507 <aby2507@gmail.com>
2024-05-17 18:38:42 -0600

    se agrego euroka

--- games-service/src/main/java/com/msyc/gamesservice/GamesServiceApplication.java
+++ games-service/src/main/java/com/msyc/gamesservice/GamesServiceApplication.java
@@ -2,8 +2,11 @@ package com.msyc.gamesservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GamesServiceApplication {
 
 	public static void main(String[] args) {
